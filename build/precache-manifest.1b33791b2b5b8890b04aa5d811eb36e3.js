c730a2781f24604a91440c5e0bceef288d431e1b
shivaramakrishna <varkalashivaramakrishna@gmail.com>
2020-11-21 22:12:46 +0530

    font update

---
+++ build/precache-manifest.1b33791b2b5b8890b04aa5d811eb36e3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0b12bd1eac9309323c1c5bc2765847",
+    "url": "/iamshivaramakrishna/Keeper-clone/index.html"
+  },
+  {
+    "revision": "1bda362f0e1ec39b0872",
+    "url": "/iamshivaramakrishna/Keeper-clone/static/js/2.8295690d.chunk.js"
+  },
+  {
+    "revision": "bc438d058e9d57ccabe9",
+    "url": "/iamshivaramakrishna/Keeper-clone/static/js/main.031a563c.chunk.js"
+  },
+  {
+    "revision": "e505d971249d0cd70c74",
+    "url": "/iamshivaramakrishna/Keeper-clone/static/js/runtime-main.b79f1b07.js"
+  }
+]);
